06bffffef4cf12c8f72178a1148162a84372aa2e
SenQii <alawaji.p@gmail.com>
2024-10-22 17:28:07 +0300

    Clerk integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import googleLogo from '@/assets/google.png';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
@@ -13,20 +14,28 @@ import {
 } from '@/components/ui/card';
 import { AlertCircle, Lock, Unlock, Plus, Send } from 'lucide-react';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-// TODO: Clerk integration
+import {
+  SignedIn,
+  SignInButton,
+  SignedOut,
+  UserButton,
+  SignIn,
+  useClerk,
+} from '@clerk/clerk-react';
+// add reanimted? add some animations to the page
 
 function App() {
-  const [activeTab, setActiveTab] = useState('create');
+  const [activeTab, setActiveTab] = useState('join');
   const [groupUrl, setGroupUrl] = useState('');
   const [questions, setQuestions] = useState([{ question: '', answer: '' }]);
   const [secureJoinUrl, setSecureJoinUrl] = useState('');
   const [joinUrl, setJoinUrl] = useState('');
   const [quizAnswers, setQuizAnswers] = useState([]);
+  const { openSignIn } = useClerk();
 
   const handleAddQuestion = () => {
     setQuestions([...questions, { question: '', answer: '' }]);
   };
-
   const handleQuestionChange = (
     index: number,
     field: string,
@@ -56,7 +65,7 @@ function App() {
       className='min-h-screen bg-gradient-to-b from-gray-50 to-white'
       dir='rtl'
     >
-      <header className='bg-white shadow-sm'>
+      <header className='bg-white shadow-sm flex flex-row'>
         <div className='container mx-auto px-4 py-6'>
           <h1 className='text-3xl font-bold text-gray-900 flex items-center'>
             <Lock className='w-8 h-8 ml-2 text-blue-600' />
@@ -66,6 +75,49 @@ function App() {
             أمّن مجموعتك عبر بوابة الانضمام الآمنة
           </p>
         </div>
+        <div className='mx-auto flex items-center justify-center'>
+          <SignedOut>
+            {/* UX: on loading, play a modal note to prompte user to sign in ++++ change button  */}
+            <div
+              className='w-7 h-7 sm:h-10 sm:w-10 mx-auto cursor-pointer'
+              onClick={() => openSignIn()}
+            >
+              <div className=''>
+                <svg
+                  version='1.1'
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 48 48'
+                  xmlnsXlink='http://www.w3.org/1999/xlink'
+                  className='block'
+                >
+                  <path
+                    fill='#EA4335'
+                    d='M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z'
+                  ></path>
+                  <path
+                    fill='#4285F4'
+                    d='M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z'
+                  ></path>
+                  <path
+                    fill='#FBBC05'
+                    d='M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z'
+                  ></path>
+                  <path
+                    fill='#34A853'
+                    d='M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z'
+                  ></path>
+                  <path fill='none' d='M0 0h48v48H0z'></path>
+                </svg>
+              </div>
+              <span className='hidden'>Sign in with Google</span>
+            </div>
+          </SignedOut>
+          <SignedIn>
+            <div className='w-7 h-7 sm:h-10 sm:w-10 mx-auto cursor-pointer'>
+              <UserButton />
+            </div>
+          </SignedIn>
+        </div>
       </header>
 
       <main className='container mx-auto px-4 py-12' dir='rtl'>
@@ -74,17 +126,18 @@ function App() {
           onValueChange={setActiveTab}
           className='w-full max-w-4xl mx-auto'
         >
-          <TabsList className='grid w-full grid-cols-2 mb-8'>
-            {/* Create Link Only for authintcaated users *** considring use Clerk? */}
-            <TabsTrigger value='create' className='text-lg py-3'>
-              <Lock className='w-5 h-5 mr-2' />
-              انشئ رابط
-            </TabsTrigger>
-            <TabsTrigger value='join' className='text-lg py-3'>
-              <Unlock className='w-5 h-5 mr-2' />
-              انضم لمجموعة عبر الرابط
-            </TabsTrigger>
-          </TabsList>
+          <SignedIn>
+            <TabsList className='grid w-full bg grid-cols-2 mb-8'>
+              <TabsTrigger value='create' className='text-lg py-3'>
+                <Lock className='w-5 h-5 mr-2' />
+                انشئ رابط
+              </TabsTrigger>
+              <TabsTrigger value='join' className='text-lg py-3'>
+                <Unlock className='w-5 h-5 mr-2' />
+                انضم لمجموعة عبر الرابط
+              </TabsTrigger>
+            </TabsList>
+          </SignedIn>
           <TabsContent value='create'>
             <Card className='border-t-4 border-t-blue-600' dir='rtl'>
               <CardHeader>
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+
+const CLERK_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!CLERK_KEY) {
+  // for any reason, the key is not available
+  throw new Error('Clerk key is missing');
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={CLERK_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>
 );
