e91e5be86b6961139670b5d0d149d115a2ef8a5f
sho <alawaji.p@gmail.com>
2025-04-07 13:09:32 +0300

    refactor: enhance Dashboard component layout and styling; update service worker revision

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m6n82p53nko"
+    "revision": "0.3prp1g3p21g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
